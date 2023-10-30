8bfe0d856f0c0db877d7ca797d86bc767fa41bff
“Francisco” <allandouglasmello@gmail.com>
2023-10-30 13:00:46 -0300

    Realizado escrita para excel, nomes dos cursos

--- bot.py
+++ bot.py
@@ -1,6 +1,32 @@
 from bs4 import BeautifulSoup
 from scrapingant_client import ScrapingAntClient
+import os, xlsxwriter, cleantext
 
+def criaplanilha(nomes, links):
+    arquivo = os.path.exists("cursos.xlsx")
+    if arquivo == False:
+        workbook = xlsxwriter.Workbook("teste.xlsx")
+        worksheet = workbook.add_worksheet()
+        row = 0
+        column = 0
+        tempstring = ""
+        print(nomes)
+        
+        for i, l in enumerate(nomes):
+            tempstring = ""
+            for j, col in enumerate(l):
+                tempstring += str(nomes[i][j])
+                tempstring += " "
+            tempstring = tempstring.split("=")
+            worksheet.write(i, column, str(tempstring[1]))
+        #for i in range(len(links)):
+         #   for j in range(len(links[i])):
+          #      worksheet.write(row, col + 1, links[i][j])
+           # row += 1
+        #worksheet.write(row, 0, 'Total')
+        #worksheet.write(row, 1, '=SUM(B1:B4)')
+        workbook.close()
+    
 # Define a url para buscar o conteudo do site
 url = "https://cursos.intelbras.com.br/portal/layout/927/intelbras/home.asp?V29ya3NwYWNlSUQ9MTI2NSZjRmlsdHJvPSRNb2RvPWdyaWQmRmlsdHJvcyRQYWdpbmE6MSZQb3JQYWdpbmE6MjAmQ2F0ZWdvcmlhc0ZpbHRyYWRhc0AkaWQ9MTImbm9tZT1Db211bmljYSVDMyVBNyVDMyVBM28mY2F0ZWdvcmlhX2lkPTcmY2F0ZWdvcmlhX25vbWU9VW5pZGFkZSZxdGQ9Nzc7JiRpZD05NyZub21lPUdyYXR1aXRvJmNhdGVnb3JpYV9pZD0xNCZjYXRlZ29yaWFfbm9tZT1Db25kaSVDMyVBNyVDMyVBM28mcXRkPTM0OzsmQnVzY2FUZXJtbzpudWxsJlRpcG9GaWx0cmFkbz1Ub2RvcyZXb3Jrc3BhY2VJRDoxMjY1Jmt0X2RpZGF4aXM9dG9w#"
 
@@ -15,7 +41,48 @@ soup = BeautifulSoup(page_content, features= "html5lib")
 #encontre todos elementos da pagina com tag <a> e classe "link-produto", corresponde as grades dos cursos
 textoHTML = soup.find_all("a" ,class_="link-produto")
 #itera entre elementos da pagina, cada elemento corresponde a um curso mostrado na pagina, cada um possui link e nome
+listaresultado = []
+fim = False
+j = 0
+listanomes = []
+listalinks = []
 for i in range(len(textoHTML)):
-    print(textoHTML[i], i)
+    
+    fixedstring = str(textoHTML[i])
+    fixedstring = fixedstring.replace("<", "").replace(">", "").replace("*", "").replace("?", "")
+    fixedstring = fixedstring.split()
+    n = 3
+    tempnomelist = []
+    templinklist = []
+    
+    x = range(n, len(fixedstring))
+    for n in x:
+        if "href" not in fixedstring[n]:
+            
+            tempnomelist.append(fixedstring[n])
+        else:
+            
+            templinklist.append(fixedstring[n])
+            break
+        n += 1
+    listanomes.append(tempnomelist)
+    listalinks.append(templinklist)
+    
+    #listaresultado.append(fixedstring[])
+    #listaresultado.append(fixedstring[3])
+
+listanomesfix = [] 
+for i in listanomes: 
+    if i not in listanomesfix: 
+        listanomesfix.append(i) 
+
+listalinksfix = [] 
+for i in listalinks: 
+    if i not in listalinksfix: 
+        listalinksfix.append(i) 
+        
+
+criaplanilha(listanomesfix, listalinksfix)
+
 
 
